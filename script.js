ec075b6acebdaf2b57ac7fbc063d3eb136a6368f
Andrey <skvaer76@gmail.com>
2019-03-06 18:06:52 +0200

    Это наш первий комит

---
+++ script.js
@@ -0,0 +1,36 @@
+(function (){
+    'use strict';
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(second);
+
+let person = {
+    name: "Jone",
+    age: 25,
+    isMarried: true
+};
+
+console.log(person.name);
+
+// var add = ["piter", "hitin", "mikki"];
+
+
+// console.log(add[0]);
+
+// let answer1 = +prompt("Are you +18", "yes");
+
+// console.log(typeof (answer1));
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(incr);
+console.log(decr);
+
+
+}());
